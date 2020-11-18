2229a0c223d444020f02f51212e32448390bb80f
Andre Suchitra <andre.suchitra@mekari.com>
2020-11-18 10:28:15 +0700

    netlify only accept https call

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://ec2-13-212-201-235.ap-southeast-1.compute.amazonaws.com:3001',
+  baseURL: 'https://ec2-13-212-201-235.ap-southeast-1.compute.amazonaws.com:3001',
 });
