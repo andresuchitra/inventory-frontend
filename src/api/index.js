dfcc523f1a8956cd6db41caa1a36be98264356c8
Andre Suchitra <43630527+andresuchitra@users.noreply.github.com>
2020-11-18 11:11:48 +0700

    include port

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://ec2-13-212-201-235.ap-southeast-1.compute.amazonaws.com',
+  baseURL: 'https://ec2-13-212-201-235.ap-southeast-1.compute.amazonaws.com:3001',
 });
